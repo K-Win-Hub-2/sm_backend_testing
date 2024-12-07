25a3169f96cd797e8cee089218fd725f2ce7a585
realkhatju <programmercruso15@gmail.com>
2024-12-07 13:16:54 +0630

    teacher category

--- app/Models/TeacherCategory.php
+++ app/Models/TeacherCategory.php
@@ -2,14 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\teacher;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class TeacherCategory extends Model
 {
     use HasFactory;
     protected $guarded = [];
     public function teachers(){
-        return $this->hasMany(Teacher::class);
+        return $this->hasMany(teacher::class);
     }
 }
